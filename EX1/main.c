1a2a4520adf8d826a34dc43cc5c930cfb99185a2
kaodomi <151603952+kaodomi@users.noreply.github.com>
2026-09-24 13:55:13 +0800

    Update main.c

--- EX1/main.c
+++ EX1/main.c
@@ -2,7 +2,7 @@
 int main() {
     printf("\t\t2025 9月\n");
     printf("日\t一\t二\t三\t四\t五\t六\n");
-    printf("\033[1;34m");
+    printf("\033[1;34m31");
     printf("\033[1;36m\t 1\t 2\t 3\t 4\t 5\t 6\n");
     printf(" 7\t 8\t 9\t10\t11\t12\t13\n");
     printf("14\t15\t16\t17\t18\t19\t20\n");
